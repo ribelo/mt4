f0d356530a98688f1b165014422040259358448a
huxley <huxley.source@gmail.com>
2014-02-04 18:26:42 +0100

    Add wrb_management module

---
+++ src/wrb_management.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "math.h"
+#include "wrb_struct.h"
+#include "candle.h"
+
+
+double support(ohlc *candle, size_t i, int hg_only, int use_fractal,
+               size_t l, int n);
+
+
+double resistance(ohlc *candle, size_t i, int hg_only, int use_fractal,
+                  size_t l, int n);
